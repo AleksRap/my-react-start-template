ff729aaa9977831ab988cba3dee597828cb7ceab
Седов Алексей <aleks.sedov@mail.ru>
2022-02-03 11:42:10 +0300

    init project

---
+++ src/components/App/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import styles from './styles.module.scss';
+
+export const App: FC = () => {
+  return (
+    <div className={styles.app}>
+      <span>Hello world</span>
+    </div>
+  );
+}
